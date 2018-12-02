9b26e687eba1d7e028274687e6065f027517ffe6
oliwalker <ofwalker@live.co.uk>
2018-12-02 23:39:48 +0100

    added React Spring, animated everything

--- views/Portfolio/Portfolio.js
+++ views/Portfolio/Portfolio.js
@@ -1,24 +1,29 @@
+import React from 'react';
 import Project from './Project';
 import myProjectList from '../../myProjectList';
 
-const Portfolio = () => (
-  <div className="portfolio">
-    {Object.values(myProjectList).map(project => (
-      <Project project={project} key={Object.keys(project)} />
-    ))}
-
-    <style jsx>
-      {`
-        .portfolio {
-          width: 100vw;
-          height: ${Object.keys(myProjectList).length * 94}vh;
-        }
-        h1 {
-          padding-left: 3vw;
-        }
-      `}
-    </style>
-  </div>
-);
+class Portfolio extends React.PureComponent {
+  render(){
+    const { projectNumber } = this.props
+    return (
+      <div className="portfolio">
+        {Object.values(myProjectList).map((project, i) => (
+          <Project project={project} projectNumber={projectNumber} key={Object.keys(project)} i={i} />
+        ))}
+        <style jsx>
+          {`
+            .portfolio {
+              width: 100vw;
+              height: ${Object.keys(myProjectList).length * 94}vh;
+            }
+            h1 {
+              padding-left: 3vw;
+            }
+          `}
+        </style>
+      </div>
+    );
+  }
+}
 
 export default Portfolio;
