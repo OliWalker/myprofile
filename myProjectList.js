043f883a0315bbc3c9456e205be187510902b9a0
oliwalker <ofwalker@live.co.uk>
2018-12-03 18:37:39 +0100

    reduced the image size

--- myProjectList.js
+++ myProjectList.js
@@ -14,11 +14,11 @@ module.exports = {
       'Also included - performance visualisation so you can see your progress over time.',
     techStack: ['React', 'Redux', 'Koa', 'Maria Db'],
     picLinks: [
-      'https://res.cloudinary.com/dh9xnvxbz/image/upload/v1535643683/localhost_3001__Pixel_2__pixel_quite_black_portrait.png',
-      'https://res.cloudinary.com/dh9xnvxbz/image/upload/v1535643683/profile_pixel_quite_black_portrait.png',
-      'https://res.cloudinary.com/dh9xnvxbz/image/upload/v1535643683/sports_pixel_quite_black_portrait.png',
-      'https://res.cloudinary.com/dh9xnvxbz/image/upload/v1535643683/challenge_pixel_quite_black_portrait.png',
-      'https://res.cloudinary.com/dh9xnvxbz/image/upload/v1535643683/league_pixel_quite_black_portrait.png'
+      'https://res.cloudinary.com/dh9xnvxbz/image/upload/q_30/v1535643683/localhost_3001__Pixel_2__pixel_quite_black_portrait.png',
+      'https://res.cloudinary.com/dh9xnvxbz/image/upload/q_30/v1535643683/profile_pixel_quite_black_portrait.png',
+      'https://res.cloudinary.com/dh9xnvxbz/image/upload/q_30/v1535643683/sports_pixel_quite_black_portrait.png',
+      'https://res.cloudinary.com/dh9xnvxbz/image/upload/q_30/v1535643683/challenge_pixel_quite_black_portrait.png',
+      'https://res.cloudinary.com/dh9xnvxbz/image/upload/q_30/v1535643683/league_pixel_quite_black_portrait.png'
     ],
     demo: 'https://versus.oliwalker.me'
   },
@@ -33,13 +33,13 @@ module.exports = {
     keyFeatures1:
       'Free Your Stuff is a progressive web app that enables people to efficiently and easily upload their unwanted items up for the public to take.',
     keyFeatures2:
-      'It allows for very fast uploading and automatic "tagging" with image recognition technology',
+      'It allows for very fast uploading and automatic "tagging" with image recognition technology.',
     techStack: ['React', 'Redux', 'Koa', 'Mongo Db'],
     picLinks: [
-      'https://res.cloudinary.com/dh9xnvxbz/image/upload/v1535559541/localhost_3000__Pixel_2__pixel_very_silver_portrait.png',
-      'https://res.cloudinary.com/dh9xnvxbz/image/upload/v1535559541/localhost_3000_add_Pixel_2__pixel_very_silver_portrait.png',
-      'https://res.cloudinary.com/dh9xnvxbz/image/upload/v1535559542/localhost_3000__Pixel_2_2__pixel_very_silver_portrait.png',
-      'https://res.cloudinary.com/dh9xnvxbz/image/upload/v1535559542/localhost_3000__Pixel_2_3__pixel_very_silver_portrait.png'
+      'https://res.cloudinary.com/dh9xnvxbz/image/upload/q_30/v1535559541/localhost_3000__Pixel_2__pixel_very_silver_portrait.png',
+      'https://res.cloudinary.com/dh9xnvxbz/image/upload/q_30/v1535559541/localhost_3000_add_Pixel_2__pixel_very_silver_portrait.png',
+      'https://res.cloudinary.com/dh9xnvxbz/image/upload/q_30/v1535559542/localhost_3000__Pixel_2_2__pixel_very_silver_portrait.png',
+      'https://res.cloudinary.com/dh9xnvxbz/image/upload/q_30/v1535559542/localhost_3000__Pixel_2_3__pixel_very_silver_portrait.png'
     ],
     demo: 'https://freeyourstuff.oliwalker.me'
   },
@@ -52,13 +52,13 @@ module.exports = {
     keyFeatures1:
       'Deed is a Web App that allows the user to create communities, and reward their members with karma points for various online and offline task',
     keyFeatures2:
-      ' Collected karma points can later be exchanged for different custom prizes, facilitating the creation of a micro-economy within the community',
+      ' Collected karma points can later be exchanged for different custom prizes, facilitating the creation of a micro-economy within the community.',
     techStack: ['Angular', 'GraphQl', 'Mongo Db'],
     picLinks: [
-      'https://res.cloudinary.com/dh9xnvxbz/image/upload/v1535644512/Screen_Shot_2018-08-30_at_17.24.32_pixel_quite_black_portrait.png',
-      'https://res.cloudinary.com/dh9xnvxbz/image/upload/v1535644512/Screen_Shot_2018-08-30_at_17.32.00_pixel_quite_black_portrait.png',
-      'https://res.cloudinary.com/dh9xnvxbz/image/upload/v1535644512/Screen_Shot_2018-08-30_at_17.32.31_pixel_quite_black_portrait.png',
-      'https://res.cloudinary.com/dh9xnvxbz/image/upload/v1535644512/Screen_Shot_2018-08-30_at_17.34.43_pixel_quite_black_portrait.png'
+      'https://res.cloudinary.com/dh9xnvxbz/image/upload/q_30/v1535644512/Screen_Shot_2018-08-30_at_17.24.32_pixel_quite_black_portrait.png',
+      'https://res.cloudinary.com/dh9xnvxbz/image/upload/q_30/v1535644512/Screen_Shot_2018-08-30_at_17.32.00_pixel_quite_black_portrait.png',
+      'https://res.cloudinary.com/dh9xnvxbz/image/upload/q_30/v1535644512/Screen_Shot_2018-08-30_at_17.32.31_pixel_quite_black_portrait.png',
+      'https://res.cloudinary.com/dh9xnvxbz/image/upload/q_30/v1535644512/Screen_Shot_2018-08-30_at_17.34.43_pixel_quite_black_portrait.png'
     ]
   }
 };
